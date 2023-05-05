d864c3f0f98d2e6613428877c05ad10c1a58c51f
Gustavo "Goose" Grau Bolivar <gustavo.graubolivar@kellogg.northwestern.edu>
2023-05-05 21:37:19 +0000

    All done with the in-class exercise

---
+++ app/controllers/salespeople_controller.rb
@@ -0,0 +1,2 @@
+class SalespeopleController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources "tacos"
   resources "dice"
+  resources "companies"
+  resources "contacts"
+  resources "salespeople"
+  resources "activities"
 end
